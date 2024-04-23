e8318dc19cfe5d98b4bf0550e94d29cbd544f99b
karlapasaran <pasarankarla6@gmail.com>
2024-04-22 21:28:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cotizacinesfiestas/DatosPer.java
@@ -0,0 +1,14 @@
+package com.example.cotizacinesfiestas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosPer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_per);
+    }
+}
